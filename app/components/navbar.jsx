7fc9c6b2cd75a4e83a70af46de33e0520134ea65
emiliojuni0r <emiliohamsyahjr@gmail.com>
2025-10-01 12:07:04 +0700

    add: essentials route for customer side,

---
+++ app/components/navbar.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navbar(){
+    const pathname = usePathname();
+    const isAuthRoute =
+    pathname === "/sign-in" ||
+    pathname === "/sign-up" ||
+    pathname === "/forgot-pass";
+
+    return(
+        <div className="w-full h-24 flex flex-row justify-between items-center px-6 shadow-md">
+            {/* logo */}
+            <Link href={"/"}>
+                <div className="text-3xl">ini logo</div>
+            </Link>
+
+            {/* two button container */}
+            <div className="flex flex-row space-x-4 text-2xl">
+                <Link className="" href={"/order"}><div className="rounded-lg bg-white border-[#FFD966] border-[3px] p-2 hover:bg-gray-50 active:bg-gray-100">Pesanan</div></Link>
+                <Link className="" href={"/profile"}><div className="rounded-lg bg-white border-[#FFD966] border-[3px] p-2 hover:bg-gray-50 active:bg-gray-100">Profil</div></Link>
+            </div>
+        </div>
+    )
+}
