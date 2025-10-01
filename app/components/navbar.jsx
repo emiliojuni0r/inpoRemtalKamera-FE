86b17873b6b21c06a103e4cade61fdd99ebe45a2
emiliojuni0r <emiliohamsyahjr@gmail.com>
2025-10-01 13:15:02 +0700

    fix : navbar behavior (hidden in auth routes)

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -3,25 +3,29 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function Navbar(){
+export default function Navbar() {
     const pathname = usePathname();
     const isAuthRoute =
-    pathname === "/sign-in" ||
-    pathname === "/sign-up" ||
-    pathname === "/forgot-pass";
+        pathname === "/login" ||
+        pathname === "/register" ||
+        pathname === "/forgot-pass";
 
-    return(
-        <div className="w-full h-24 flex flex-row justify-between items-center px-6 shadow-md">
-            {/* logo */}
-            <Link href={"/"}>
-                <div className="text-3xl">ini logo</div>
-            </Link>
+    return (
+        <>
+            {!isAuthRoute && (
+                <div className="w-full h-24 flex flex-row justify-between items-center px-6 shadow-md">
+                    {/* logo */}
+                    <Link href={"/"}>
+                        <div className="text-3xl">ini logo</div>
+                    </Link>
 
-            {/* two button container */}
-            <div className="flex flex-row space-x-4 text-2xl">
-                <Link className="" href={"/order"}><div className="rounded-lg bg-white border-[#FFD966] border-[3px] p-2 hover:bg-gray-50 active:bg-gray-100">Pesanan</div></Link>
-                <Link className="" href={"/profile"}><div className="rounded-lg bg-white border-[#FFD966] border-[3px] p-2 hover:bg-gray-50 active:bg-gray-100">Profil</div></Link>
-            </div>
-        </div>
+                    {/* two button container */}
+                    <div className="flex flex-row space-x-4 text-2xl">
+                        <Link className="" href={"/order"}><div className="rounded-lg bg-white border-[#FFD966] border-[3px] p-2 hover:bg-gray-50 active:bg-gray-100">Pesanan</div></Link>
+                        <Link className="" href={"/profile"}><div className="rounded-lg bg-white border-[#FFD966] border-[3px] p-2 hover:bg-gray-50 active:bg-gray-100">Profil</div></Link>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
